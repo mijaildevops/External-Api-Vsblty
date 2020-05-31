ed4e496716aa0a8a29d238f8f00ea03eb2dff1a5
Mijail <Mijail>
2020-05-31 18:08:39 -0500

    Python App Backend and List Html Funtion javascript

--- Backend/Python/Api.py
+++ Backend/Python/Api.py
@@ -15,17 +15,77 @@ import uuid
 # Cors
 from flask_cors import CORS
 
+# Mkdir
+from shutil import rmtree 
+from os import makedirs
+from os import remove
+import shutil
+
 app=Flask(__name__,template_folder='templates')
 cors = CORS(app)
 
+#//////////////////////////////////////////////////////////////////////////
+# Index
+#//////////////////////////////////////////////////////////////////////////
 @app.route('/')
 def home():
-    return 'Api rest External'
+    return 'Api Rest External'
 
-# Token
-@app.route('/Token/<UserId>')
-def TokenData(UserId):
+#//////////////////////////////////////////////////////////////////////////
+# User
+#//////////////////////////////////////////////////////////////////////////
+
+#///////////////////////////////////////
+# UserData
+#///////////////////////////////////////
+@app.route('/User/<UserId>')
+def User(UserId):
     #print (EndpointId)
+    connection = pymysql.connect(host='192.168.100.51',
+        user='Qatest',
+        password='Quito.2019',
+        db='External-Api',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor)
+
+
+    try:
+        with connection.cursor() as cursor:
+           
+            sql2 = "SELECT `User`, `Endpoint_Id`, `Environment` FROM `User` WHERE `Id`=%s"
+            cursor.execute(sql2, (UserId))
+            result = cursor.fetchone()
+            
+            try:
+
+                User = str(result.get('User'))
+                Endpoint_Id = str(result.get('Endpoint_Id'))
+                Environment = int(result.get('Environment'))
+
+                if (Environment == 1):
+                    Url = "https://api.vsblty.net/"
+                else:
+                    Url = "https://vsblty-apiv2-qa.azurewebsites.net/"
+            
+                connection.commit()
+                Message = "Successful"
+
+                return jsonify([{'Environment': Url, 'Message': Message, 'User': User, 'Endpoint_Id': Endpoint_Id}])
+            except:
+                Message = "Error, Peticion Negada"
+                return jsonify([{'Message': Message}])
+
+    finally:
+        connection.close()
+
+#//////////////////////////////////////////////////////////////////////////
+# Generar Token
+#//////////////////////////////////////////////////////////////////////////
+@app.route('/Token/<UserId>')
+def Token(UserId):
+
+    UserId = int(UserId)
+    
     connection = pymysql.connect(host='192.168.100.51',
         user='Qatest',
         password='Quito.2019',
@@ -40,20 +100,24 @@ def TokenData(UserId):
             cursor.execute(sql, (UserId))
             result = cursor.fetchone()
 
-
+            connection.commit()
 
             grant_type = str(result.get('grant_type'))
             client_id = str(result.get('client_id'))
             client_secret = str(result.get('client_secret'))
-            Environment = str(result.get('Environment'))
+            Environment = int(result.get('Environment'))
 
-            result = [grant_type, client_id, client_secret, Environment]
+            if (Environment == 1):
+                Url = "https://api.vsblty.net/"
+            else:
+                Url = "https://vsblty-apiv2-qa.azurewebsites.net/"
 
-            grant_type = result[0]
-            client_id = result[1]
-            client_secret = result[2]
-            Environment_Url = result[3] + "/token"
-            
+            print (grant_type)
+            print (client_id)
+            print (client_secret)
+            print (Url)
+
+            Environment_Url = Url + "/token"
 
             #Realizamos peticion Http
             pload = {'grant_type':grant_type,'client_id':client_id,'client_secret':client_secret}
@@ -65,35 +129,78 @@ def TokenData(UserId):
             #Decoded respuesta
             decoded = json.loads(data_string)
 
-            # capturamos Variables
-            Token = str(decoded["access_token"])
-            Generado = str(decoded[".issued"])
-            Expira = str(decoded[".expires"])
+                # capturamos Variables
+            try:
+                Token = str(decoded["access_token"])
+                Generado = str(decoded[".issued"])
+                Expira = str(decoded[".expires"])
+                Message = "Token generated correctly... (Expires in 1 Hour)"
+                error = ""
+            except:
+                error = str(decoded["error"])
 
+            if len(error) > 0:
+                return jsonify([{'Message': error}])
+            else:
 
-            # Actualizar todos los registos del Usuario
-            sql_update_query = """UPDATE Token set IsActive = %s where User_Id = %s"""
-            data_tuple = (0, UserId)
+                # Actualizar todos los registos del Usuario
+                sql_update_query = """DELETE FROM Token where User_Id = %s"""
+                data_tuple = (UserId)
+                cursor.execute(sql_update_query, data_tuple)
+                connection.commit()
 
-            cursor.execute(sql_update_query, data_tuple)
-            connection.commit()
-
-            # Insertar \             
-            sql = "INSERT INTO `Token` (`User_Id`, `Token`, `Toke_Generated`, `Token_Expiration`) VALUES (%s, %s, %s, %s)"
-            cursor.execute(sql, (UserId, Token, Generado, Expira))
-            
-            # connection is not autocommit by default. So you must commit to save
-            # your changes.
-            connection.commit()
-
-        return jsonify("Se ha Generado Token Correctamente..")
+                # Insertar 
+                sql = "INSERT INTO `Token` (`User_Id`, `Token`, `Toke_Generated`, `Token_Expiration`) VALUES (%s, %s, %s, %s)"
+                cursor.execute(sql, (UserId, Token, Generado, Expira))
+                connection.commit()
+                
+                # Mensaje 
+                return jsonify([{'Environment': Url, 'Message': Message, 'Token': Token, 'Generated': Generado, 'Expires': Expira}])
+                # Actualizar todos los registos del Usuario
+           
+            connection.commit() 
 
     finally:
         connection.close()
 
-        # Agregar Token Funcion AddToken
+#//////////////////////////////////////////////////////////////////////////
+# Token Data
+#//////////////////////////////////////////////////////////////////////////
+@app.route('/Generated/<UserId>')
+def TokenData(UserId):
+    #print (EndpointId)
+    connection = pymysql.connect(host='192.168.100.51',
+        user='Qatest',
+        password='Quito.2019',
+        db='External-Api',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor)
 
-# Mostartr data
+    try:
+        with connection.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT `Token`, `Toke_Generated`, `Token_Expiration` FROM `Token` WHERE `User_Id`=%s"
+            cursor.execute(sql, (UserId))
+            result = cursor.fetchone()
+
+            # commit to save
+            connection.commit()
+
+            try:
+                Token = str(result.get('Token'))
+                Toke_Generated = str(result.get('Toke_Generated'))
+                Token_Expiration = str(result.get('Token_Expiration'))
+                Message = "Data Succesful"
+                return jsonify([{'Message': Message, 'Token': Token, 'Generated': Toke_Generated, 'Expires': Token_Expiration}])
+            except:
+                Message = "Error Pericion Invalida"
+                return jsonify([{'Message': Message}])
+
+    finally:
+        connection.close()
+
+
+# Get data One
 @app.route('/Data/<UserId>')
 def GetData(UserId):
 
@@ -114,7 +221,7 @@ def GetData(UserId):
     try:
         with connection.cursor() as cursor:
             # Read a single record
-            sql = "SELECT `Token` FROM `Token` WHERE `User_Id`=%s AND IsActive=%s "
+            sql = "SELECT `Token` FROM `Token` WHERE `User_Id`=%s AND IsActive=%s"
             cursor.execute(sql, (UserId, 1))
             result = cursor.fetchone()
             Token = str(result.get('Token'))
@@ -129,84 +236,92 @@ def GetData(UserId):
             result = cursor.fetchone()
 
             Endpoint_Id = str(result.get('Endpoint_Id'))
-            Environment = str(result.get('Environment'))
+            Environment = int(result.get('Environment'))
+
+            if (Environment == 1):
+                Environment = "https://api.vsblty.net/"
+            else:
+                Environment = "https://vsblty-apiv2-qa.azurewebsites.net/"
 
             Url = Environment + '/api/LiveEndpointData/'+ Endpoint_Id
 
-            #pload = {'Authorization' : 'Bearer' + 'Token 90JATsV1lIYYXuH44jyfwkrpTiPv0eGxo_2FD4aqgKyiNUjzA56D7vXZG25tvV6jFjhoCF8NuoG0SgwzL3PVSPTcRCRT3PbWqULOhpl8FtVfe1whTjolBM-1iafgRiQKaRAO85CfO0x1Mwh9G8HtXZjzTfvylx4ajkzZ8upCD_dXrSXCQg8MHH_nHYDu47-DZ9XyzFOIAt9qJQjHf3jpUiPQNjKHmVwAQy17u3wENUVS4g8VrL0nBo76XEGshVyp7zXR428KnuMgjb4HjP_F1g'}
-            r = requests.post(Url, headers=headers)
-            print(r.text)
+            try:
+                #pload = {'Authorization' : 'Bearer' + 'Token 90JATsV1lIYYXuH44jyfwkrpTiPv0eGxo_2FD4aqgKyiNUjzA56D7vXZG25tvV6jFjhoCF8NuoG0SgwzL3PVSPTcRCRT3PbWqULOhpl8FtVfe1whTjolBM-1iafgRiQKaRAO85CfO0x1Mwh9G8HtXZjzTfvylx4ajkzZ8upCD_dXrSXCQg8MHH_nHYDu47-DZ9XyzFOIAt9qJQjHf3jpUiPQNjKHmVwAQy17u3wENUVS4g8VrL0nBo76XEGshVyp7zXR428KnuMgjb4HjP_F1g'}
+                r = requests.post(Url, headers=headers)
+                print(r.text)
 
-            dir = 'C:/Pruebas'  # También es válido 'C:\\Pruebas' o r'C:\Pruebas'
-            file_name = Guid +".json"
+                try:
+                    # change the destination path
+                    dir = "C:/Pruebas/"  +str(UserId) + "/" 
+                    makedirs(dir)
+                    print (" - Creating Folder of Test-", UserId)
+                except FileExistsError:
+                    print (" - Folder Exists")
+                    dir = "C:/Pruebas/"  +str(UserId) + "/"
+                
+                
+                file_name =  Guid +".json"
 
-            with open(os.path.join(dir, file_name), 'w') as file:
-                json.dump(r.json(), file)
-        
+                with open(os.path.join(dir, file_name), 'w') as file:
+                    json.dump(r.json(), file)
+
+                return jsonify(r.json())
+            except:
+                Message = "Error Pericion Invalida, Verify the Token and Url Environment"
+                return jsonify([{'Message': Message}])
                  
-        return jsonify(r.json())
+        
 
     finally:
         connection.close()
 
-@app.route('/User/<Idtest>')
-def User(Idtest):
-    #print (EndpointId)
-    connection = pymysql.connect(host='192.168.100.51',
-        user='Qatest',
-        password='Quito.2019',
-        db='External-Api',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor)
+# Get Data List
+@app.route('/DataList/<UserId>')
+def GetDataList(UserId):
+    Path = "C:/Pruebas/" + str(UserId) + "/"
+    dirs = os.listdir(Path)
+    print (Path)
+    print (len(dirs))
+    DataList = []
 
+    for file in dirs:
+        
+        File = Path + file
+        print ("file: ", file)
+        print ("Path: ", File)
 
-    try:
-        with connection.cursor() as cursor:
+        with open(File) as contenido:
+            DataFile = json.load(contenido)
 
-            #///////////////////////////////
+            #encoded
+            data_string = json.dumps(DataFile)
+
+            #Decoded
+            decoded = json.loads(data_string)
+
+            Image = str(decoded["CapturedImageURL"])
+            Name = str(decoded["EndpointData"]["IdentityName"])
+            Match = str(decoded["EndpointData"]["Confidence"])
+            Age = str(decoded["EndpointData"]["Age"])
+            Gender = str(decoded["EndpointData"]["Gender"])
+            FrameTime = str(decoded["EndpointData"]["FrameTime"])
+            #print (Image)
+            print (Image)
             
-            sql2 = "SELECT `User`, `Endpoint_Id`, `Environment` FROM `User` WHERE `Id`=%s"
-            cursor.execute(sql2, (Idtest))
-            resultMensajes_Actual = cursor.fetchall()
-            #print(resultMale)
-            
-            #///////////////////////////////
-         
-
-        return jsonify(resultMensajes_Actual)
-
-    finally:
-        connection.close()
-
-@app.route('/TokenInfo/<Idtest>')
-def TokenInfo(Idtest):
-    #print (EndpointId)
-    connection = pymysql.connect(host='192.168.100.51',
-        user='Qatest',
-        password='Quito.2019',
-        db='External-Api',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor)
+            GetDataFile = {
+                    "file":file,
+                    "Image":Image,
+                    "Name":Name,
+                    "Match":Match,
+                    "Age":Age,
+                    "Gender":Gender,
+                    "FrameTime":FrameTime,
+                    }
+            DataList.append(GetDataFile)
 
 
-    try:
-        with connection.cursor() as cursor:
-
-            #///////////////////////////////
-            
-            sql = "SELECT `Id`, `Toke_Generated`, `Token_Expiration`, `Time` FROM `Token` WHERE `User_Id`=%s AND `IsActive`=%s  "
-            cursor.execute(sql, (Idtest, 1))
-            resultMensajes_Actual = cursor.fetchall()
-            #print(resultMale)
-            
-            #///////////////////////////////
-         
-
-        return jsonify(resultMensajes_Actual)
-
-    finally:
-        connection.close()
+    return jsonify(DataList)
 
 if __name__ == '__main__':
-    #app.run(host='192.168.100.233', port=5080, debug=True)
-    app.run(host='192.168.100.51', port=5080, debug=True)
+    app.run(host='192.168.100.233', port=5080, debug=True)
+    
