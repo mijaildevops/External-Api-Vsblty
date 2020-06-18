8a65964c21c7d7e80cb4ba785c78882374e219bd
Mijail <Mijail>
2020-06-18 18:43:12 -0500

    Request LiveEndpoint-Data
    While True

--- Backend/Python/Request.py
+++ Backend/Python/Request.py
@@ -7,8 +7,9 @@ import uuid
 from shutil import rmtree 
 from os import makedirs
 from os import remove
-import shutil
+import time
 
+print ("errrrrrrrrrrrrrrrorrrrrrrrr")
 #///////////////////////////////////////////
 # Generamos Un GUID 
 #///////////////////////////////////////////
@@ -25,75 +26,78 @@ client_id = "83A472BC-4BC3-4C77-AFE1-F4EF4F51B13B"
 client_secret = "auOfdE4A0Id53bZvR9PBHBgKttQocdg4Xwk1caiTQEUxmSOS32NqbXx4tThynSr3uFT5bGKvaCHwpC4SnOYRoQ=="
 Url = "https://apivnext.vsblty.net/"
 # Endpoint
-Endpoint_Id = '4120a492-3206-47d9-93b1-f885accffd52' # Tim
-Endpoint_Id = '5abb3937-e91c-4881-a0b1-568abaf027d6'
+Endpoint_Id = 'b6b03e48-bef3-4341-adec-3fec8c303d73'
 # Url Token
 Environment_Url = Url + "/token"
 # Url Liveendpointdata
 UrlData = Url + "/api/LiveEndpointData/" +Endpoint_Id
 
-#///////////////////////////////////////////
-# token Request
-#///////////////////////////////////////////
-pload = {'grant_type':grant_type,'client_id':client_id,'client_secret':client_secret}
-r = requests.post(Environment_Url, data = pload)
 
-#encoded respuesta
-data_string = json.dumps(r.json())
+while True:
 
-#Decoded respuesta
-decoded = json.loads(data_string)
+    time.sleep(60)
+    #///////////////////////////////////////////
+    # token Request
+    #///////////////////////////////////////////
+    pload = {'grant_type':grant_type,'client_id':client_id,'client_secret':client_secret}
+    r = requests.post(Environment_Url, data = pload)
 
-# capture token variables
-try:
-    Token = str(decoded["access_token"])
-    Generado = str(decoded[".issued"])
-    Expira = str(decoded[".expires"])
-    Message = "Token generated correctly... (Expires in 1 Hour)"
-    error = ""
-    print ("(Token) -OK -The token was generated correctly")
-    #print ("Token:", Token)
-except:
-    error = str(decoded["error"])
-    Token = None
-    print ("(Token) -ERROR -The token was not generated")
+    #encoded respuesta
+    data_string = json.dumps(r.json())
 
-if (Token is not None):
-    print ("the token is not null")
+    #Decoded respuesta
+    decoded = json.loads(data_string)
 
-    # build request with token on head
-    Token = 'Bearer ' + Token
-    headers = {'Authorization' :  Token  }
+    # capture token variables
     try:
-        # request to obtain the data that the Endpoint is viewing
-        r = requests.post(UrlData, headers=headers)
-        #print(r.json)
-        print()
-        print ("The request to Live-endpoint-Data was successful")
-
-        # GUID to identify the file
-        GUID = GuidFile ()
-        print ("  -File: ", GUID)
-
-        # a folder is created to store the response of the json files
-        try:
-            # the destination path
-            dir = "C:/LiveEndpointData/"  
-            makedirs(dir)
-            print ("   -- Creating Folder of Test-")
-        except FileExistsError:
-            print ("   -- Folder Exists")
-            dir = "C:/LiveEndpointData/" 
-
-        # We save the request response with the data in the json file
-        file = "C:/LiveEndpointData/" + str(GUID) + ".json"
-        path = "C:/LiveEndpointData/" + str(GUID) + ".json"
-        with open(os.path.join(dir, file), 'w') as file:
-            json.dump(r.json(), file) 
-            print ("   -- FILE: the data was saved in the file: ", path)
-
+        Token = str(decoded["access_token"])
+        Generado = str(decoded[".issued"])
+        Expira = str(decoded[".expires"])
+        Message = "Token generated correctly... (Expires in 1 Hour)"
+        error = ""
+        print ("(Token) -OK -The token was generated correctly")
+        #print ("Token:", Token)
     except:
-        print ("Error getting the data")
-        
-else:
-    print ("ERROR -the token is null")
+        error = str(decoded["error"])
+        Token = None
+        print ("(Token) -ERROR -The token was not generated")
+
+    if (Token is not None):
+        print ("the token is not null")
+
+        # build request with token on head
+        Token = 'Bearer ' + Token
+        headers = {'Authorization' :  Token  }
+        try:
+            # request to obtain the data that the Endpoint is viewing
+            r = requests.post(UrlData, headers=headers)
+            #print(r.json)
+            print()
+            print ("The request to Live-endpoint-Data was successful")
+
+            # GUID to identify the file
+            GUID = GuidFile ()
+            print ("  -File: ", GUID)
+
+            # a folder is created to store the response of the json files
+            try:
+                # the destination path
+                dir = "C:/LiveEndpointData/"  
+                makedirs(dir)
+                print ("   -- Creating Folder of Test-")
+            except FileExistsError:
+                print ("   -- Folder Exists")
+                dir = "C:/LiveEndpointData/" 
+
+            # We save the request response with the data in the json file
+            file = "C:/LiveEndpointData/" + str(GUID) + ".json"
+            path = "C:/LiveEndpointData/" + str(GUID) + ".json"
+            with open(os.path.join(dir, file), 'w') as file:
+                json.dump(r.json(), file) 
+                print ("   -- FILE: the data was saved in the file: ", path)
+
+        except:
+            print ("Error getting the data")
+            
+    else:
+        print ("ERROR -the token is null")
